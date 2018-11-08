e608f5790604ae2bb27ca9a85d00395498b5e6bb
SANJAY K SAI <shachsan@hotmail.com>
2018-11-08 19:18:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       self.status = "complete"
     else
       "Transaction rejected. Please check your account balance."
-      self.status =
+      self.status = "rejected"
     end
   end
 
